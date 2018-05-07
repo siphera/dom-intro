ed0918d0cca84d97d7af2b4695321694d1557df2
siphera <salmansiphenkosi@gmail.com>
2018-05-07 12:00:01 +0200

    separated logic from dom

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,55 +1,54 @@
-var callsTotalradio = 0.00;
-var smsTotalradio = 0.00;
+document.addEventListener('DOMContentLoaded', function() 
+{
+
 
 // get a reference to the sms or call radio buttons
+
 var callsTotalElem = document.querySelector(".callTotalTwo");
 var smsTotalElem = document.querySelector(".smsTotalTwo");
-
 
 //get a reference to the add button
 
 var RadioTotalAddBtn = document.querySelector(".radioBillAddBtn");
 //create a variable that will keep track of the total bill
-
 var totalCostElem = document.querySelector(".totalTwo");
 var billTypeRadio = document.querySelector(".billItemTypeRadio");
 
+
+var radioFactoryF = radioBillFactoryF();
 //add an event listener for when the add button is pressed
-
-//in the event listener get the value from the billItemTypeRadio radio buttons
-// * add the appropriate value to the running total
-// * add nothing for invalid values that is not 'call' or 'sms'.
-
-
-RadioTotalAddBtn.addEventListener('click', function(){
-
+RadioTotalAddBtn.addEventListener('click', 
+function()
+{
+   
    var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
    var billItemType = checkedRadioBtn.value;
     // update the correct total
-    console.log(billItemType);
-    if (billItemType === "call"){
-        callsTotalradio += 2.75
-    }
-    else if (billItemType === "sms"){
-        smsTotalradio += 0.75;
-    }
 
-    // * display the latest total on the screen
-    callsTotalElem.innerHTML = callsTotalradio.toFixed(2);
-    smsTotalElem.innerHTML = smsTotalradio.toFixed(2);
-    var totalCostradio = callsTotalradio + smsTotalradio;
-    totalCostElem.innerHTML = totalCostradio.toFixed(2);
-
-
-
-    //color the total according to the total
-    if (totalCostradio >= 50){
-
+    
+    //update the totals that is displayed on the screen.
+    var radioAdd = radioFactoryF.radioCalculate(billItemType);
+    var callsTotals = radioFactoryF.callTotal(); 
+    var smsTotals = radioFactoryF.smsTotal();
+    var sum = radioFactoryF.totalBill();
+    
+    
+    callsTotalElem.innerHTML = callsTotals;
+    smsTotalElem.innerHTML = smsTotals;
+    totalCostElem.innerHTML = sum;
+    
+    
+      
+    //color the total based on the criteria
+    if (sum >= 50){
+        // adding the danger class will make the text red
         totalCostElem.classList.add("danger");
     }
-    else if (totalCostradio >= 30){
+    else if (sum >= 30){
         totalCostElem.classList.add("warning");
     }
+    });
+
 });
 
 function fieldReset(){
